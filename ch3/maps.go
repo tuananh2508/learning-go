ac2243ef1be1afca436eae2077008e5b2d60f598
MyPC <tuananh2508999@gmail.com>
2021-12-18 21:18:56 +0700

    Update

--- ch3/maps.go
+++ ch3/maps.go
@@ -1,12 +1,22 @@
 package main
 
-import fmt
+import "fmt"
 
 func main() {
-	teams := map[string][]string {
-		"Orcas": []string{"Fred", "Ralph", "Bijou"},
-		"Lions": []string{"Sarah", "Peter", "Billie"},
+	m := make(map[string]int)
+	m["n1"] = 1
+	m["n2"] = 2
+	a := m["n1"]
+	b := m["n2"]
+	fmt.Println(a, b)
+
+	teams := map[string][]string{
+		"Orcas":   []string{"Fred", "Ralph", "Bijou"},
+		"Lions":   []string{"Sarah", "Peter", "Billie"},
 		"Kittens": []string{"Waldo", "Raul", "Ze"},
 	}
-	fmt.Println(teams)
+	team1 := teams["Orcas"]
+	team2 := teams["Lions"]
+	team3 := teams["Kittens"]
+	fmt.Println(team1, team2, team3)
 }
