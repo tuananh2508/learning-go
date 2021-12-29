305d173aeaf303c9c7547eeee089ffd35b9cc00e
MyPC <tuananh2508999@gmail.com>
2021-12-29 21:26:50 +0700

    Update

--- ch3/struct.go
+++ ch3/struct.go
@@ -14,5 +14,33 @@ func main() {
 		age:  22,
 		name: "tuananh",
 	}
-	fmt.Println(ta.name)
+	fmt.Println(ta.name, ta.age)
+
+	// can compare the same type of data with different struct
+	// defind first struct
+	type first_person struct {
+		name string
+		age  int
+		job  string
+	}
+	//sample values
+	bob := first_person{
+		name: "bob",
+		age:  22,
+		job:  "driver",
+	}
+	//define sencond struct
+	type second_person struct {
+		name string
+		age  int
+		job  string
+	}
+	//sample values
+	alice := second_person{
+		name: "alice",
+		age:  23,
+		job:  "teacher",
+	}
+	//compare same type of data with 2 structs
+	fmt.Println(bob.age != alice.age)
 }
